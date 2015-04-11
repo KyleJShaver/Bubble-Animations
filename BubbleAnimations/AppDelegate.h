d43894e2bcf323a0e6fa7ed525041ea270de2134
Kyle Shaver <kyleshaver@me.com>
2015-04-11 09:47:57 -0700

    Initial Commit

---
+++ BubbleAnimations/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BubbleAnimations
+//
+//  Created by Kyle Shaver on 4/11/15.
+//  Copyright (c) 2015 Team Wireframe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BubbleAnimations/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  BubbleAnimations
+//
+
+//  Copyright (c) 2015 Team Wireframe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
